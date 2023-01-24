f962976d4816a5cd51771e82578a2bd94d3d6e11
hadlaczkyb <hadlaczkybzs@gmail.com>
2023-01-24 21:24:11 +0100

    Updates

---
+++ precache-manifest.7fbee47025d1dd7b8b46b6dc4bd15f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2a71e8fe3b5999f68599dc44859b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a7e94fb6f2e915692e4",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8516b0fde62714862018",
+    "url": "/static/js/2.a08838e2.chunk.js"
+  },
+  {
+    "revision": "5a7e94fb6f2e915692e4",
+    "url": "/static/js/main.4d29494e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "699182b69d15007bd4850de1c649403a",
+    "url": "/static/media/profile.699182b6.png"
+  }
+]);
